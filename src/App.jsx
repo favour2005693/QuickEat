21fc9976fa9348052f7dde332d7ef28dc696ae75
Emmanuel Michael Favour <favouremma215@gmail.com>
2026-05-15 17:24:13 +0100

    Navbar setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+// import ProfileForm from "./pages/ProfileForm/ProfileForm";
 function App() {
-  return <></>;
+  return (
+    <>
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/menu" element={<h1>Menu</h1>} />
+        <Route path="/about" element={<h1>About</h1>} />
+        <Route path="/contact" element={<h1>Contact</h1>} />
+      </Routes>
+    </BrowserRouter>
+
+    {/* <ProfileForm /> */}
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,243 @@
+// Imports
+import { useState } from "react";
+import {
+  Menu,
+  X,
+  ShoppingCart,
+  Search,
+  User,
+  Moon,
+  Sun,
+  ChevronDown,
+} from "lucide-react";
+import { Link } from "react-router-dom";
+import logo from "../assets/images/logo.png";
+import profileImage from "../assets/images/profile.jpg"
+
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  // Toggle Dark Mode
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+
+    if (!darkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  };
+
+  return (
+    <nav className="sticky top-0 z-50 bg-white dark:bg-white-600 shadow-md transition duration-300">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        {/* Navbar Container */}
+        <div className="flex justify-between items-center h-16">
+          {/* Logo */}
+          <Link to="/" className="flex items-center gap-2">
+            <img src={logo}  className="w-40 "alt="logo" />
+          </Link>
+
+          {/* Desktop Navigation */}
+          <div className="hidden md:flex items-center gap-8">
+            <Link
+              to="/"
+              className="text-black-700  hover:text-orange-500 transition duration-300"
+            >
+              Home
+            </Link>
+
+            <Link
+              to="/menu"
+              className="text-black-700 hover:text-orange-500 transition duration-300"
+            >
+              Menu
+            </Link>
+            <Link
+              to="/about"
+              className="text-black-700 hover:text-orange-500 transition duration-300"
+            >
+              About
+            </Link>
+
+            <Link
+              to="/contact"
+              className="text-black-700  hover:text-orange-500 transition duration-300"
+            >
+              Contact
+            </Link>
+
+            {/* Dropdown */}
+            <div className="relative">
+              <button
+                onClick={() => setDropdownOpen(!dropdownOpen)}
+                className="flex items-center gap-1 text-black-700 hover:text-orange-500 transition"
+              >
+                Pages
+                <ChevronDown size={18} />
+              </button>
+
+              {dropdownOpen && (
+                <div className="absolute top-12 left-0 w-44 bg-white dark:bg-white-700 shadow-lg rounded-xl p-3 flex flex-col gap-3">
+                  <Link
+                    to="/offers"
+                    className="hover:text-red-500 dark:text-black"
+                  >
+                    Offers
+                  </Link>
+
+                  <Link
+                    to="/orders"
+                    className="hover:text-red-500 dark:text-black"
+                  >
+                    Orders
+                  </Link>
+
+                  <Link
+                    to="/profile"
+                    className="hover:text-red-500 dark:text-black"
+                  >
+                    Profile
+                  </Link>
+                </div>
+              )}
+            </div>
+          </div>
+
+          {/* Right Side */}
+          <div className="hidden md:flex items-center gap-4">
+            {/* Search */}
+            <button className="p-2 rounded-full hover:bg-gray-100 dark:hover:bg-gray-800 transition">
+              <Search size={20} className="text-gray-700 dark:text-white" />
+            </button>
+
+            {/* Dark Mode */}
+            <button
+              onClick={toggleDarkMode}
+              className="p-2 rounded-full hover:bg-gray-100 dark:hover:bg-gray-800 transition"
+            >
+              {darkMode ? (
+                <Sun size={20} className="text-yellow-400" />
+              ) : (
+                <Moon size={20} className="text-gray-700" />
+              )}
+            </button>
+
+            {/* Cart */}
+            <button className="relative p-2 rounded-full hover:bg-gray-100 dark:hover:bg-gray-800 transition">
+              <ShoppingCart
+                size={22}
+                className="text-gray-900  dark:text-black"
+              />
+
+              <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs w-5 h-5 rounded-full flex items-center justify-center">
+                3
+              </span>
+            </button>
+
+            {/* User */}
+            <button className="rounded-full hover:bg-gray-100">
+              {/* <User size={22} className="text-gray-700 dark:text-white" /> */}
+              <img class="w-12 cursor-pointer h-12 rounded-full " src={profileImage} alt="user Profile" />
+            </button>
+
+            {/* CTA Button */}
+            <button className="bg-red-500 hover:bg-red-600 text-white px-5 py-2 rounded-full font-medium transition duration-300">
+             Sign Up
+            </button>
+          </div>
+
+          {/* Mobile Menu Button */}
+          <button
+            className="md:hidden text-gray-700 dark:text-black"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            {isOpen ? <X size={30} /> : <Menu size={30} />}
+          </button>
+        </div>
+      </div>
+
+      {/* Mobile Menu */}
+      {isOpen && (
+        <div className="md:hidden bg-white dark:bg-white-900 shadow-lg">
+          <div className="flex flex-col gap-4 px-6 py-5">
+            <Link
+              to="/"
+              className="text-gray-700 dark:text-black hover:text-orange-500"
+            >
+              Home
+            </Link>
+
+            <Link
+              to="/menu"
+              className="text-gray-700 dark:text-black hover:text-orange-500"
+            >
+              Menu
+            </Link>
+
+            <Link
+              to="/about"
+              className="text-gray-700 dark:text-black hover:text-orange-500"
+            >
+              About
+            </Link>
+
+            <Link
+              to="/contact"
+              className="text-gray-700 dark:text-black hover:text-orange-500"
+            >
+              Contact
+            </Link>
+
+            <Link
+              to="/offers"
+              className="text-gray-700 dark:text-black hover:text-orange-500"
+            >
+              Offers
+            </Link>
+
+            {/* Mobile Icons */}
+            <div className="flex items-center gap-4 pt-4 border-t border-gray-200 dark:border-gray-700">
+              <button className="p-2 rounded-full hover:bg-gray-100 dark:hover:bg-gray-800">
+                <Search size={20} className="text-black-700 dark:text-black" />
+              </button>
+
+              <button
+                onClick={toggleDarkMode}
+                className="p-2 rounded-full hover:bg-gray-100 dark:hover:bg-gray-800"
+              >
+                {darkMode ? (
+                  <Sun size={20} className="text-yellow-400" />
+                ) : (
+                  <Moon size={20} className="text-gray-700 dark:text-black" />
+                )}
+              </button>
+
+              <button className="relative p-2 rounded-full hover:bg-gray-100 dark:hover:bg-gray-800">
+                <ShoppingCart
+                  size={22}
+                  className="text-black-700 dark:text-black"
+                />
+
+                <span className="absolute -top-1 -right-1 bg-orange-500 text-white text-xs w-5 h-5 rounded-full flex items-center justify-center">
+                  3
+                </span>
+              </button>
+
+              <button className="p-2 rounded-full hover:bg-gray-100 dark:hover:bg-gray-800">
+                <User size={22} className="text-gray-700 dark:text-black" />
+              </button>
+            </div>
+
+            {/* Mobile Button */}
+            <button className="bg-orange-500 hover:bg-orange-600 text-white py-2 rounded-full font-medium transition duration-300">
+              Sign Up
+            </button>
+          </div>
+        </div>
+      )}
+    </nav>
+  );
+}
